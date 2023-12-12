fc177550dae3b045771266e2b48f238e86954ccb
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2023-12-12 22:18:08 +0500

    Deleted

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_3
-# Build directory: C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_3/build/search_engine_3[1]_include.cmake")
-subdirs("_deps/googletest-build")
-subdirs("nlohmann_json")
